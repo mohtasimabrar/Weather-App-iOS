c93693cd5b69768cfaca5cdeb9d2ba57d5203755
Mohtasim Abrar Samin <mohtasim.abrar@gmail.com>
2024-06-27 21:09:11 +0600

    First commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Samin on 27/6/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
